5858482a137a5b077d4e8a401532d97d159ba8de
A-no-maliya <126778037+A-no-maliya@users.noreply.github.com>
2024-04-07 21:20:49 +0300

    Update matrix_operations.h

--- laba_5_4sem/matrix_operations.h
+++ laba_5_4sem/matrix_operations.h
@@ -1,1 +1,17 @@
 #pragma once
+#pragma once
+#ifndef MATRIX_OPERATIONS_H
+#define MATRIX_OPERATIONS_H
+
+#include <vector>
+#include <fstream>
+
+struct Matrix {
+    std::vector<std::vector<int>> data;
+};
+
+void writeMatrixToFile(const std::string& filename, const std::vector<Matrix>& matrices);
+std::vector<Matrix> readMatricesFromFile(const std::string& filename);
+
+#endif
+
